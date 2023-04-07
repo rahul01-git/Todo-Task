a0a721b7a460792401b1163235c8574ab2f7c41a
rahul01-git <rahulneupane0090@gmail.com>
2023-04-07 23:48:43 +0545

    fixing

--- script.js
+++ script.js
@@ -4,45 +4,40 @@ const tbody = document.querySelector('tbody');
 let count = 1;
 const todos = JSON.parse(localStorage.getItem('todos') || '[]');
 
-window.addEventListener('load',()=>{
-    todos.forEach(todo=>{
+window.addEventListener('load', () => {
+    todos.forEach(todo => {
         const tr = document.createElement('tr');
         const td1 = document.createElement('td');
-        const td2 = document.createElement('td');
         const td = document.createElement('td');
         tr.appendChild(td1);
         tr.appendChild(td);
-        tr.appendChild(td2);
         td.innerText = todo;
         td1.innerText = count++;
-        td2.innerHTML = "<button class='btn dlt' onclick='deleteTodo()'>x</button>";
+
         tbody.append(tr);
     })
 
 })
 
-btn.addEventListener('click',()=>{
+btn.addEventListener('click', () => {
     let todo = inputVal.value;
+    if (!todo) alert('empty');
+    else {
 
-    todos.push(todo);
-    localStorage.setItem('todos',JSON.stringify(todos));
+        todos.push(todo);
+        localStorage.setItem('todos', JSON.stringify(todos));
 
-    const tr = document.createElement('tr');
-    const td1 = document.createElement('td');
-    const td2 = document.createElement('td');
-    const td = document.createElement('td');
-    td1.innerText = count++;
-    tr.appendChild(td1);
-    tr.appendChild(td);
-    tr.appendChild(td2);
-    td.innerText = todo;
-    td2.innerHTML = "<button class='btn dlt' onclick='deleteTodo()'>x</button>";
-    todos.forEach(todo => {
-        tbody.append(tr);
-    });
+        const tr = document.createElement('tr');
+        const td1 = document.createElement('td');
+        const td = document.createElement('td');
+        td1.innerText = count++;
+        tr.appendChild(td1);
+        tr.appendChild(td);
+        td.innerText = todo;
+        todos.forEach(todo => {
+            tbody.append(tr);
+        });
+    }
 
 })
 
-function deleteTodo(){
-    alert('dlt pressed');
-}
