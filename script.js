c2c5a754d89be345a1276d503a2e2c2243d13dd9
rahul01-git <rahulneupane0090@gmail.com>
2023-04-08 00:19:36 +0545

    dlt btn works

--- script.js
+++ script.js
@@ -9,8 +9,15 @@ window.addEventListener('load', () => {
         const tr = document.createElement('tr');
         const td1 = document.createElement('td');
         const td = document.createElement('td');
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.addEventListener('click', () => {
+          deleteFromLocalStorage(td1.innerText);
+          tr.remove();
+        });
         tr.appendChild(td1);
         tr.appendChild(td);
+        tr.appendChild(deleteBtn);
         td.innerText = todo;
         td1.innerText = count++;
 
@@ -23,16 +30,22 @@ btn.addEventListener('click', () => {
     let todo = inputVal.value;
     if (!todo) alert('empty');
     else {
-
         todos.push(todo);
         localStorage.setItem('todos', JSON.stringify(todos));
 
         const tr = document.createElement('tr');
         const td1 = document.createElement('td');
         const td = document.createElement('td');
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.addEventListener('click', () => {
+          deleteFromLocalStorage(td1.innerText);
+          tr.remove();
+        });
         td1.innerText = count++;
         tr.appendChild(td1);
         tr.appendChild(td);
+        tr.appendChild(deleteBtn);
         td.innerText = todo;
         todos.forEach(todo => {
             tbody.append(tr);
@@ -41,3 +54,8 @@ btn.addEventListener('click', () => {
 
 })
 
+function deleteFromLocalStorage(key) {
+    todos.splice(key-1,1);
+    localStorage.setItem('todos',JSON.stringify(todos))
+
+  }
